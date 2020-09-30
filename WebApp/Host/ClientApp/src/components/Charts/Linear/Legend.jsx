f4e81f24c814efd1cde878510f2ef453acd84997
VIsaev <vladimir.isaev@zyfra.com>
2020-09-30 16:32:49 +0300

    #легенда в линейной диаграмме

---
+++ WebApp/Host/ClientApp/src/components/Charts/Linear/Legend.jsx
@@ -0,0 +1,20 @@
+﻿import React from 'react';
+import map from 'lodash/map';
+import noop from 'lodash/noop';
+import includes from 'lodash/includes';
+
+const LegendItem = ({id, name, color, hidden, onClick}) => {
+  return (
+    <div onClick={() => onClick(id)} >
+      <div style={{backgroundColor: color, height: '20px', width: '20px', opacity: hidden ? '20%' : '100%'}}/>
+      <span style={{ opacity: hidden ? '20%' : '100%' }} >{name}</span>
+    </div>
+  );
+};
+
+export default ({dataSource = [], hiddenElementsId = [], onClick = noop}) => {
+  return map(dataSource, sourceItem => {
+    const hidden = hiddenElementsId.length > 0 ? includes(hiddenElementsId, sourceItem.id) : false
+    return <LegendItem {...sourceItem} hidden={hidden} onClick={onClick}/>
+  });
+};
--- WebApp/Host/ClientApp/src/components/Charts/Linear/index.jsx
+++ WebApp/Host/ClientApp/src/components/Charts/Linear/index.jsx
@@ -1,28 +1,51 @@
-﻿import React, { useMemo } from 'react';
+﻿import React, {useMemo, useState} from 'react';
 import useMeasure from 'react-use/lib/useMeasure';
+import includes from 'lodash/includes'
+import filter from 'lodash/filter'
 
 import LinearChartContext from './LinearChartContext'
-import { normalizeProps } from './utils'
+import {normalizeProps} from './utils'
 
 import HorizontalLayout from './HorizontalLayout';
 import VerticalLayout from './VerticalLayout';
 import Series from './Series';
+import Legend from './Legend';
 
-export default ({ dataSource }) => {
-  const [ref, { width, height }] = useMeasure();
+import styles from './index.less'
+
+export default ({dataSource}) => {
+  const [ref, {width, height}] = useMeasure();
+  const [hiddenElementsId, setHiddenElementsId] = useState([]);
 
   const props = useMemo(() => {
-    return normalizeProps(dataSource, width, height);
-  }, [dataSource, width, height])
+    if (hiddenElementsId.length > 0) {
+      const filteredDataSource = filter(dataSource, x => !includes(hiddenElementsId, x.id));
+      return normalizeProps(filteredDataSource, width, height);
+    } else
+      return normalizeProps(dataSource, width, height);
+  }, [dataSource, width, height, hiddenElementsId])
+
+  const legendOnClick = (id) => {
+    if (includes(hiddenElementsId, id)) {
+      setHiddenElementsId([...filter(hiddenElementsId, x => x !== id)]);
+    } else {
+      setHiddenElementsId([...hiddenElementsId, id])
+    }
+  }
 
   return (
-    <div ref={ref}>
+    <div>
       <LinearChartContext.Provider value={props}>
-        <svg key='linearChart' style={{ width: '100%', height: `${props.height}px`, fontSize: 12 }}>
-          <HorizontalLayout />
-          <VerticalLayout />
-          <Series />
-        </svg>
+        <div ref={ref}>
+          <svg key='linearChart' style={{width: '100%', height: `${props.height}px`, fontSize: 12}}>
+            <HorizontalLayout/>
+            <VerticalLayout/>
+            <Series/>
+          </svg>
+        </div>
+        <div className={styles.legendContainer} >
+          <Legend onClick={legendOnClick} dataSource={dataSource} hiddenElementsId={hiddenElementsId}/>
+        </div>
       </LinearChartContext.Provider>
     </div>
   );
